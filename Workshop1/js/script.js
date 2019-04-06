b3f1bf7267ebcbc260400b506effccf420a82390
匿名 劉 <yeelm1487@gmail.com>
2019-04-06 23:16:19 +0800

    46

--- Workshop1/js/script.js
+++ Workshop1/js/script.js
@@ -16,6 +16,7 @@ function loadBookData() {
     if (bookDataFromLocalStorage == null) {
         bookDataFromLocalStorage = bookData;
         localStorage.setItem('bookData', JSON.stringify(bookDataFromLocalStorage));
+        
     }
 }
 //把英文書本種類改成中文
@@ -51,13 +52,24 @@ function AddThousand() {
         bookDataFromLocalStorage[i].BookTotal = FormatNumber(bookDataFromLocalStorage[i].BookTotal);
         bookDataFromLocalStorage[i].BookTotal = bookDataFromLocalStorage[i].BookTotal + "元";
     }
-
 }
+var a="";
+function AddFontawesome() {
+    for (var i in bookDataFromLocalStorage) {
+        if (bookDataFromLocalStorage[i].BookDeliveredDate != "") {
+             a = "fas fa-couch";
+        }
+    }
+}
+
+
+
 
 $(function () {
     loadBookData();
     ChangeCategory();
-    AddThousand()
+    AddThousand();
+    AddFontawesome()
 });
 
     $(document).ready(function () {
@@ -73,36 +85,74 @@ $(function () {
                             BookAuthor: { type: "string" },
                             BookBoughtDate: { type: "string" },
                             BookDeliveredDate: { type: "string" },
-                            BookPrice: { type: "string"},
+                            BookPrice: { type: "string" },
                             BookAmount: { type: "number" },
-                            BookTotal: {type :"string"}
+                            BookTotal: { type: "string" }
                         }
                     }
                 },
                 pageSize: 20
             },
-            height: 550,
+            height: 600,
             width: 600,
             scrollable: true,
             sortable: true,
             filterable: true,
+            editable: true,
             pageable: {
                 input: true,
                 numeric: false
             },
             columns: [
-               
+                { command: [{ className: "btn-destroy", name: "destroy", text: "刪除" }], width: "60px" },
                 { field: "BookId", title: "ID", width: "60px" },
                 { field: "BookName", title: "書名", width: "210px" },
-                { field: "BookCategory", title: "種", width: "100px" },
-                { field: "BookAuthor", title: "作者", width: "130px" },
-                { field: "BookBoughtDate", title: "購買日期", width: "100px" },
-                { field: "BookDeliveredDate", title: "送達狀態", width: "100px" },
-                { field: "BookPrice", title: "金額", width: "60px" },
-                { field: "BookAmount", title: "數量", width: "60px" },
+                { field: "BookCategory", title: "種類", width: "80px" },
+                { field: "BookAuthor", title: "作者", width: "100px" },
+                { field: "BookBoughtDate", title: "購買日期", width: "90px" },
+                { field: "BookDeliveredDate", title: "送達狀態", width: "70px"},
+                { field: "BookPrice", title: "金額", width: "70px" },
+                { field: "BookAmount", title: "數量", width: "70px" },
                 { field: "BookTotal", title: "總計", width: "70px"},
 
             ]
         });
-    });
+});
+//這裡是搜尋text box
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}  //req
+
+function getBoolean(str) {
+    if ("true".startsWith(str)) {
+        return true;
+    } else if ("false".startsWith(str)) {
+        return false;
+    } else {
+        return null;
+    }
+}
+
+    $('#filter').on('input', function (e) {//req
+        var grid = $('#book_grid').data('kendoGrid');
+        var columns = grid.columns;
+
+        var filter = { logic: 'or', filters: [] };
+        columns.forEach(function (x) {
+            if (x.field) {
+                var type = grid.dataSource.options.schema.model.fields[x.field].type;
+                if (type == 'string') {
+                    filter.filters.push({
+                        field: x.field,
+                        operator: 'contains',
+                        value: e.target.value
+                    })
+                }
+            }
+        });
+        grid.dataSource.filter(filter);
+    
+});
+//這裡是搜尋text box
+
                 
